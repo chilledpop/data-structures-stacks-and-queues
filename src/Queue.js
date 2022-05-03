dd2611934ea1e8e032b061c503d7629d007dba61
Thaomai Nguyen <thaomaicnguyen@gmail.com>
2022-05-02 20:42:50 -0700

    submitted

---
+++ src/Queue.js
@@ -0,0 +1,42 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (this.first) {
+      this.last.next = newNode;
+    } else {
+      // Set the node of the queue's next to be the new node
+      this.first = newNode;
+    }
+
+    //make the new node the last item on the queue
+    this.last = newNode;
+  }
+
+  dequeue() {
+    if (this.first) {
+      const dequeued = this.first;
+      this.first = dequeued.next;
+
+      if (dequeued === this.last) {
+        this.last = null;
+      }
+
+      return dequeued.value;
+    }
+  }
+}
+
+module.exports = Queue;
